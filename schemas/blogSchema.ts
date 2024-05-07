6a1f6ebc647014cfba1e06f218880440bafa9878
RPerron91 <46370966+RPerron91@users.noreply.github.com>
2024-05-07 18:05:41 -0400

    first commit

---
+++ schemas/blogSchema.ts
@@ -0,0 +1,30 @@
+import {Model, Schema, model} from 'mongoose'
+
+export interface Blog {
+    Title: string,
+    Body: string,
+    Author: string,
+    Date: Date
+}
+
+const BlogSchema = new Schema({
+    Title: {
+        type: String,
+        required: true,
+    },
+
+    Body: {
+        type: String,
+        required: true,
+    },
+    Author: {
+        type: String,
+        required: true
+    },
+    Date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+export const BlogPostModel = new Model(BlogSchema)
